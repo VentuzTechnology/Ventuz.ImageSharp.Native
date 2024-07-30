ccd8a5a0dd8b194632f6bd791efad9d4b68d821a
Tammo Hinrichs <tammo.hinrichs@ventuz.com>
2024-07-30 14:58:15 +0200

    Added copyright notice to sources

--- Ventuz.ImageSharp.Native/NativeMethods.cs
+++ Ventuz.ImageSharp.Native/NativeMethods.cs
@@ -1,4 +1,24 @@
-﻿using System.Runtime.InteropServices;
+﻿/*
+ * Ventuz.ImageSharp.Native
+ * Copyright (c) 2024 Ventuz Technology <https://ventuz.com>
+ *
+ * This file is part of Ventuz.ImageSharp.Native
+ *
+ * libheif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libheif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Ventuz.ImageSharp.Native.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Runtime.InteropServices;
 
 namespace Ventuz.ImageSharp.Native;
 
--- Ventuz.Native.ImageFormats/src/heicDecoder.cpp
+++ Ventuz.Native.ImageFormats/src/heicDecoder.cpp
@@ -1,3 +1,23 @@
+/*
+ * Ventuz.Native.ImageFormats
+ * Copyright (c) 2024 Ventuz Technology <https://ventuz.com>
+ *
+ * This file is part of Ventuz.ImageSharp.Native
+ *
+ * libheif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libheif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Ventuz.ImageSharp.Native.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <string.h>
 
 #include "decoder.h"
