984c9d9bac88ed2a96279c1486d955315941e473
Tammo Hinrichs <tammo.hinrichs@ventuz.com>
2024-08-13 13:35:00 +0200

    Copyright notice fix

--- Ventuz.ImageSharp.Native/NativeFormatDetector.cs
+++ Ventuz.ImageSharp.Native/NativeFormatDetector.cs
@@ -4,15 +4,16 @@
  *
  * This file is part of Ventuz.ImageSharp.Native
  *
- * libheif is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * Ventuz.ImageSharp.Native is free software: you can redistribute 
+ * it and/or modify it under the terms of the GNU Lesser General 
+ * Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later 
+ * version.
  *
- * libheif is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Ventuz.ImageSharp.Native is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with Ventuz.ImageSharp.Native.  If not, see <http://www.gnu.org/licenses/>.
